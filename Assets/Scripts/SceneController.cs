ef7411a51e0f407c7d8d06e7074b44df56596f28
hereami79 <hereami79@gmail.com>
2024-08-31 17:05:49 +0900

    Assets

---
+++ Assets/Scripts/SceneController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SceneManager.LoadScene("GCWCard");
+
+        }
+        
+    }
+}
